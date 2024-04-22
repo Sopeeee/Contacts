e3f999f02cf5970d7769d042b8a1341c94196689
Sopeeee <150648482+Sopeeee@users.noreply.github.com>
2024-04-23 00:49:19 +0300

    Add files via upload

---
+++ name.js
@@ -0,0 +1,16 @@
+﻿const button = document.getElementById('subscribeAction');
+const input = document.getElementById('subscribeInput');
+
+button.addEventListener('click', () => {
+  setTimeout(() => {
+    if (input.value != '') {
+      alert(`сообщение отправленно на email: ${input.value}`)
+      input.value = '';
+    }
+  }, 1000);
+})
+
+const form = document.querySelector('.form')
+form.addEventListener('reset', function (evt) {
+  console.log(evt)
+})
